620189481a66f07daf277924d82dec617a78de40
Maxime Thomé <thomemaximepro@gmail.com>
2024-02-08 16:50:18 +0100

    Ajout page bootstrap
    Page en php

---
+++ Public/assets/header.php
@@ -0,0 +1,32 @@
+<header class="barre_nav">
+        <img id="Logo" src="assets/img/Barre de naviguation/LogoProjet.png" alt="Logo Classe DEVWEB 1">
+        <a class="bouton_acceuil" href="acceuil.html">
+            <div class="material-symbols-outlined">
+                home
+            </div>Acceuil
+        </a>
+
+        <a class="bouton_cours" href="acceuil.html">
+            <div class="material-symbols-outlined">
+                menu_book
+            </div>Les cours
+        </a>
+
+        <a class="bouton_rappel" href="LesGrosrappels.html">
+            <div class="material-symbols-outlined">
+                priority_high
+            </div>Rappels
+        </a>
+        <a class="bouton_citation" href="index.html">
+            <div class="material-symbols-outlined">
+                comment_bank
+            </div>Citations
+        </a>
+        <a class="bouton_info" href="https://fr.wikipedia.org/wiki/Bataille_de_Patay">
+            <div class="material-symbols-outlined">
+                info
+            </div>Infos
+        </a>
+        <a id="LogoMNS" href="https://www.metz-numeric-school.fr/fr" target="_blank"><img 
+                src="assets/img/Barre de naviguation/LogoMNS1.png" alt="Logo Metz Numeric School"></a>
+    </header>
---
+++ Public/bootstrap.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BootStrap</title>
+    <link rel="stylesheet" href="assets/footer.css">
+    <link rel="stylesheet" href="assets/cour.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" 
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+
+<body>
+    <?php require 'assets/header.php' ?>
+    <div class="principal">
+        <h1>Bootstrap</h1>
+        <br>
+        <p>Bootstrap est un framework open-source développé par Twitter. En termes simples, Bootstrap est un ensemble d'outils et de ressources qui facilitent la création de sites web et d'applications web. Il fournit une série de modèles de conception, de composants prêts à l'emploi (comme des boutons, des formulaires, des barres de navigation, etc.), ainsi que des styles CSS préétablis pour aider les développeurs à construire rapidement et efficacement des interfaces utilisateur attractives et réactives.
+        L'un des aspects les plus attrayants de Bootstrap est sa grille responsive, qui permet de créer des mises en page flexibles qui s'adaptent automatiquement à différents appareils et tailles d'écran, de sorte que vos sites web et applications fonctionnent bien sur les ordinateurs de bureau, les tablettes et les smartphones.
+        En résumé, Bootstrap simplifie le processus de développement web en fournissant des outils prêts à l'emploi et en facilitant la création d'interfaces utilisateur modernes et compatibles avec différents appareils.</p>
+        <br>
+        <p>Un framework(cadre travail) il va venir encadrer le travail es donner des outils qui vont simplifier et faciliter la production.</p>
+        <p>Bootstrap est l'un des framework les plus utiliser mondialement.</p>
+    </div>
+    <?php require 'assets/footer.php' ?>
+</body>
+</html>
